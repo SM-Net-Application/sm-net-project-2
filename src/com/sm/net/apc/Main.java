35bc65c96dc0b701dd697f5024c8bf809c0b6bbe
SM-Net-Application <36441539+SM-Net-Application@users.noreply.github.com>
2018-05-22 09:14:14 +0200

    Test

--- src/com/sm/net/apc/Main.java
+++ src/com/sm/net/apc/Main.java
@@ -29,7 +29,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-// Test
 public class Main extends Application {
 
 	public final File ICON = setIconApp();
