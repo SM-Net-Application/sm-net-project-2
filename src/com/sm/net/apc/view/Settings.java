457a0906c4f02822670b901b5233bb8bba04d7d6
SM-Net-Application <36441539+SM-Net-Application@users.noreply.github.com>
2018-06-04 15:52:46 +0200

    Added Settings Mask

---
+++ src/com/sm/net/apc/view/Settings.java
@@ -0,0 +1,28 @@
+package com.sm.net.apc.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Settings {
+
+	@FXML
+	private TextField textFieldExtension;
+	@FXML
+	private TextField textFieldMinutes;
+	@FXML
+	private Button buttonSave;
+	
+	public void initialize(){
+		
+	}
+	
+	public void init(){
+		
+	}
+	
+	public void buttonSaveOnClick(){
+		
+	}
+	
+}
